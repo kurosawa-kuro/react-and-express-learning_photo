cf31326dce3a0ea3401e62b9a9359b035ea23411
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-05-20 21:00:21 +0900

    rm isAuthenticated

--- frontend/src/hooks/Auth/useUserAuthentication.js
+++ frontend/src/hooks/Auth/useUserAuthentication.js
@@ -4,17 +4,12 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export const useUserAuthentication = () => {
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
     const navigate = useNavigate();
 
     useEffect(() => {
         const user = JSON.parse(localStorage.getItem('user'));
         if (!user) {
             navigate('/login');
-        } else {
-            setIsAuthenticated(true);
         }
     }, [navigate]);
-
-    return isAuthenticated;
 };
