fe046f64d09ce454ef3d61d10f46146a7e4e488c
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-05-21 20:30:39 +0900

    ref

--- backend/test/user.test.js
+++ backend/test/user.test.js
@@ -1,47 +1,31 @@
-// backend\test\user.test.js
-
 import request from "supertest";
 import app from "../src/app/index.js";
 import { getUserByEmail, createUser } from "../src/app/models/userModel.js";
 import { db } from "../src/database/prisma/prismaClient.js";
 
-let newUser;
-
-beforeEach(async () => {
-    // Clean up the database before each test run
-    await db.user.deleteMany({});
-});
-
-// Close the database connection after all tests have finished
-afterAll(async () => {
-    await db.user.deleteMany({});
-    await db.$disconnect();
-});
-
 describe("POST /register", () => {
+    beforeEach(async () => {
+        await db.user.deleteMany({});
+    });
+
+    afterEach(async () => {
+        await db.user.deleteMany({});
+    });
 
     it("should create a new user", async () => {
-
-        // Set up a new user
-        newUser = {
+        const newUser = {
             name: "Test User",
             password: "test123",
             email: "testuser@aaa.com"
         };
 
-        // Act
         const response = await request(app)
             .post("/register")
             .send(newUser)
             .set("Accept", "application/json");
-        console.log("newUser", newUser);
-        console.log("POST /register should create a new user response.body", response.body);
 
-
-        // Get the user from the database
         const userFromDb = await getUserByEmail(newUser.email);
 
-        // Expected user response
         const expectedUserResponse = {
             user: {
                 id: expect.any(Number),
@@ -51,7 +35,6 @@ describe("POST /register", () => {
             token: expect.any(String),
         };
 
-        // Assert
         expect(response.status).toBe(201);
         expect(response.body).toMatchObject(expectedUserResponse);
         expect(userFromDb).toMatchObject({
@@ -64,28 +47,35 @@ describe("POST /register", () => {
 
 describe("POST /login", () => {
     beforeEach(async () => {
-        // Set up a new user
-        newUser = {
+        await db.user.deleteMany({});
+
+        const newUser = {
             name: "Test User",
             password: "test123",
             email: "testuser@aaa.com"
         };
 
-        // Register the user
         await createUser(newUser);
     });
 
+    afterEach(async () => {
+        await db.user.deleteMany({});
+    });
+
     it("should login a user", async () => {
-        // Act
+        const newUser = {
+            name: "Test User",
+            password: "test123",
+            email: "testuser@aaa.com"
+        };
+
         const response = await request(app)
             .post("/login")
             .send({ email: newUser.email, password: newUser.password })
             .set("Accept", "application/json");
 
-        // Get the user from the database
         const userFromDb = await getUserByEmail(newUser.email);
 
-        // Expected user response
         const expectedUserResponse = {
             user: {
                 id: expect.any(Number),
@@ -95,7 +85,6 @@ describe("POST /login", () => {
             token: expect.any(String),
         };
 
-        // Assert
         expect(response.status).toBe(200);
         expect(response.body).toMatchObject(expectedUserResponse);
         expect(userFromDb).toMatchObject({
@@ -106,10 +95,30 @@ describe("POST /login", () => {
     });
 });
 
-
 describe("POST /logout", () => {
+    beforeEach(async () => {
+        await db.user.deleteMany({});
+
+        const newUser = {
+            name: "Test User",
+            password: "test123",
+            email: "testuser@aaa.com"
+        };
+
+        await createUser(newUser);
+    });
+
+    afterEach(async () => {
+        await db.user.deleteMany({});
+    });
+
     it("should logout a user", async () => {
-        // Log the user in
+        const newUser = {
+            name: "Test User",
+            password: "test123",
+            email: "testuser@aaa.com"
+        };
+
         const loginResponse = await request(app)
             .post("/login")
             .send({ email: newUser.email, password: newUser.password })
@@ -117,12 +126,10 @@ describe("POST /logout", () => {
 
         const token = loginResponse.body.token;
 
-        // Act
         const response = await request(app)
             .post("/logout")
             .set("Cookie", `token=${token}`);
 
-        // Assert
         expect(response.status).toBe(200);
         expect(response.body).toMatchObject({
             message: "User logged out"
