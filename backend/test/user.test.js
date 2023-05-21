f79a61d489a6db41668cdb6cb6ecaa2b3f085776
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-05-21 18:26:21 +0900

    fix test

--- backend/test/user.test.js
+++ backend/test/user.test.js
@@ -1,56 +1,130 @@
-// Path: backend/test/register.test.js
+// backend\test\user.test.js
 
 import request from "supertest";
 import app from "../src/app/index.js";
-import { createUser, getUserByEmail } from "../src/app/models/userModel.js";
+import { getUserByEmail } from "../src/app/models/userModel.js";
 import { db } from "../src/database/prisma/prismaClient.js";
 
-describe("POST /register", () => {
-    // Clean up the database before each test run
-    beforeEach(async () => {
-        await db.user.deleteMany({});
-    });
+let newUser;
 
-    // Close the database connection after all tests have finished
-    afterAll(async () => {
-        await db.$disconnect();
+beforeEach(async () => {
+    // Clean up the database before each test run
+    await db.user.deleteMany({});
+});
+
+// Close the database connection after all tests have finished
+afterAll(async () => {
+    await db.$disconnect();
+});
+
+describe("POST /register", () => {
+    beforeEach(async () => {
+        // Set up a new user
+        newUser = {
+            name: "Test User",
+            password: "test123",
+            email: "testuser@aaa.com"
+        };
     });
 
     it("should create a new user", async () => {
-        // Arrange
-        const newUser = {
-            name: "bbb",
-            password: "aaa",
-            email: "bbb@aaa.aaa"
-        };
-
         // Act
         const response = await request(app)
             .post("/register")
             .send(newUser)
             .set("Accept", "application/json");
-        console.log("response.body", response.body);
-
-        // const cookies = response.headers['set-cookie'];
-        // console.log("cookies", cookies);
 
         // Get the user from the database
         const userFromDb = await getUserByEmail(newUser.email);
 
-        // Assert
-        expect(response.status).toBe(201);
-        expect(userFromDb).toMatchObject({
-            id: expect.any(Number),
-            name: newUser.name,
-            email: newUser.email,
-        });
-        expect(response.body).toMatchObject({
+        // Expected user response
+        const expectedUserResponse = {
             user: {
                 id: expect.any(Number),
                 name: newUser.name,
                 email: newUser.email,
             },
-            token: expect.any(String)
+            token: expect.any(String),
+        };
+
+        // Assert
+        expect(response.status).toBe(201);
+        expect(response.body).toMatchObject(expectedUserResponse);
+        expect(userFromDb).toMatchObject({
+            id: expect.any(Number),
+            name: newUser.name,
+            email: newUser.email,
+        });
+    });
+});
+
+describe("POST /login", () => {
+    beforeEach(async () => {
+        // Set up a new user
+        newUser = {
+            name: "Test User",
+            password: "test123",
+            email: "testuser@aaa.com"
+        };
+
+        // Register the user
+        await request(app)
+            .post("/register")
+            .send(newUser)
+            .set("Accept", "application/json");
+    });
+
+    it("should login a user", async () => {
+        // Act
+        const response = await request(app)
+            .post("/login")
+            .send({ email: newUser.email, password: newUser.password })
+            .set("Accept", "application/json");
+
+        // Get the user from the database
+        const userFromDb = await getUserByEmail(newUser.email);
+
+        // Expected user response
+        const expectedUserResponse = {
+            user: {
+                id: expect.any(Number),
+                name: newUser.name,
+                email: newUser.email,
+            },
+            token: expect.any(String),
+        };
+
+        // Assert
+        expect(response.status).toBe(200);
+        expect(response.body).toMatchObject(expectedUserResponse);
+        expect(userFromDb).toMatchObject({
+            id: expect.any(Number),
+            name: newUser.name,
+            email: newUser.email,
+        });
+    });
+});
+
+
+describe("POST /logout", () => {
+    it("should logout a user", async () => {
+        // Log the user in
+        const loginResponse = await request(app)
+            .post("/login")
+            .send({ email: newUser.email, password: newUser.password })
+            .set("Accept", "application/json");
+
+        const token = loginResponse.body.token;
+
+        // Act
+        const response = await request(app)
+            .post("/logout")
+            .set("Cookie", `token=${token}`);
+
+        // Assert
+        expect(response.status).toBe(200);
+        expect(response.body).toMatchObject({
+            message: "User logged out"
         });
     });
 });
